9fd3c456e5627e74fa4d66105ece990d4aa34107
Alina M <alinaamoskalenko@gmail.com>
2022-09-16 13:55:14 -0700

    react router setup

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,66 +12,70 @@ const User = require('../../models/User');
 // @desc    Test route
 // @access  Public
 router.get('/', auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user);
-    } catch(err){
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route      POST api/auth
 // @desc       Authenticate user & get token
 // @access     Public
-router.post('/', [
-    body('email', 'Please include a valid email')
-    .isEmail(),
-    body('password', 'Password is required').exists()
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        const { email, password } = req.body;
-
-        try {
-            // Check for the user
-            let user = await User.findOne({ email });
-            
-            if (!user) {
-                return res.status(400).json({ errors: [{ msg: 'Invaild Credentials' }] });
-            }
-
-            const isMatch = await bcrypt.compare(password, user.password);
-            
-            if(!isMatch) {
-                return res.status(400).json({ errors: [{ msg: 'Invaild Credentials' }] });
-            }
-            // Return jsonwebtoken
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            }
-
-            jwt.sign(
-                payload, 
-                config.get('jwtSecret'),
-                { expiresIn:36000 },
-                (err, token) => {
-                    if(err) throw err;
-                    res.json({ token });
-                });
-
-        } catch(err) {
-            console.error(err.message);
-            res.status(500).send('Server error');
-        }  
-
+router.post(
+  '/',
+  [
+    body('email', 'Please include a valid email').isEmail(),
+    body('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    const { email, password } = req.body;
+
+    try {
+      // Check for the user
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invaild Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invaild Credentials' }] });
+      }
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
 );
 
 module.exports = router;
