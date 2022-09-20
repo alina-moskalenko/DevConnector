71a86af8035e047684fe16a0f1cf828da1f641e0
Alina M <alinaamoskalenko@gmail.com>
2022-09-20 14:38:41 -0700

    redux setup & alerts

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,31 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Landing />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />} />
-          </Routes>
-        </div>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className='container'>
+            <Alert />
+            <Routes>
+              <Route path='/' element={<Landing />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/login' element={<Login />} />
+            </Routes>
+          </div>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
